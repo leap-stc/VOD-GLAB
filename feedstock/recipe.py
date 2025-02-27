76ab16d4ea9d1577e592801c72a62206c5fd122c
Raphael Hagen <norlandrhagen@gmail.com>
2025-02-27 12:07:02 -0700

    recipe wip

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,44 +1,63 @@
 """
 MODIS-LAI
 """
+
 # the modified MOD15A2GFS version sourced from nasa data is provided here by BNU data link: http://globalchange.bnu.edu.cn/research/laiv061#usage
 # data managment issue: https://github.com/leap-stc/data-management/issues/188
+from dataclasses import dataclass
+
 import apache_beam as beam
-from leap_data_management_utils.data_management_transforms import (
-    get_catalog_store_urls,
-)
+import fsspec
+
+# from leap_data_management_utils.data_management_transforms import (
+#     get_catalog_store_urls,
+# )
+import numpy as np
+import pandas as pd
+import xarray as xr
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+from pangeo_forge_recipes.storage import FSSpecTarget
 from pangeo_forge_recipes.transforms import (
-    ConsolidateDimensionCoordinates,
-    ConsolidateMetadata,
     OpenURLWithFSSpec,
     OpenWithXarray,
     StoreToZarr,
 )
 
-###########################
-## Start Modifying here ###
-###########################
 input_urls = [
     f'http://globalchange.bnu.edu.cn/laiv061/global_lai_0.05/lai_8-day_0.05_{year}.nc'
     for year in np.arange(2002, 2020 + 1)
 ]
 pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
+# NOTE! this is pruned for testing
+pattern = pattern.prune(2)
 
-# Construct the pipeline.
-modis-lai = (
-    beam.Create(pattern.items())
-    | OpenURLWithFSSpec()            # Download files with fsspec and EarthAccess authentication.
-    | OpenWithXarray()               # Open each file as an xarray.Dataset.
-    | StoreToZarr(
-          store_name="modis-lai.zarr",
-          combine_dims=pattern.combine_dim_keys,
-      )
-    | ConsolidateDimensionCoordinates()  # Merge per-file coordinate info.
-    | ConsolidateMetadata()              # Merge global metadata.
-)
 
-# For local testing, run the pipeline with a Beam runner.
-if __name__ == "__main__":
-    with beam.Pipeline() as p:
-        p | pipeline
+@dataclass
+class FixTime(beam.PTransform):
+    """change day of year to datetime"""
+
+    def _fixtime(self, ds: xr.Dataset) -> xr.Dataset:
+        year = ds.time.encoding['source'].split('lai_8-day_0.05_')[1].split('.nc>')[0]
+        dates = pd.to_datetime([f'{year}-{doy}' for doy in ds.time.values], format='%Y-%j')
+        return ds.assign_coords(time=dates)
+
+    def expand(self, pcoll):
+        return pcoll | '_fixtime' >> beam.MapTuple(lambda k, v: (k, self._fixtime(v)))
+
+
+fs = fsspec.get_filesystem_class('file')()
+target_root = FSSpecTarget(fs, 'modis_LAI')
+with beam.Pipeline() as p:
+    (
+        p
+        | beam.Create(pattern.items())
+        | OpenURLWithFSSpec()
+        | OpenWithXarray()
+        | FixTime()
+        | StoreToZarr(
+            target_root='.',
+            # TODO: Determine chunking
+            store_name='MODIS_LAI.zarr',
+            combine_dims=pattern.combine_dim_keys,
+        )
+    )
