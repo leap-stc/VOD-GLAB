36d24a62b1287d3fbe13de9380e3c39698f23290
Raphael Hagen <nrhagen@Raphaels-MacBook-Air.local>
2025-03-03 12:33:53 -0700

    modis_lia update

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -7,29 +7,32 @@ MODIS-LAI
 from dataclasses import dataclass
 
 import apache_beam as beam
-import fsspec
 
 # from leap_data_management_utils.data_management_transforms import (
 #     get_catalog_store_urls,
 # )
 import numpy as np
 import pandas as pd
+import s3fs
 import xarray as xr
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.storage import FSSpecTarget
 from pangeo_forge_recipes.transforms import (
+    ConsolidateDimensionCoordinates,
+    ConsolidateMetadata,
     OpenURLWithFSSpec,
     OpenWithXarray,
     StoreToZarr,
 )
 
+# transfered from very slow http server to leap osn with rclone - transfer.sh
 input_urls = [
-    f'http://globalchange.bnu.edu.cn/laiv061/global_lai_0.05/lai_8-day_0.05_{year}.nc'
+    f'https://nyu1.osn.mghpcc.org/leap-pangeo-manual/MODIS_LAI/lai_8-day_0.05_{year}.nc'
     for year in np.arange(2002, 2020 + 1)
 ]
+
+
 pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
-# NOTE! this is pruned for testing
-pattern = pattern.prune(2)
 
 
 @dataclass
@@ -45,8 +48,12 @@ class FixTime(beam.PTransform):
         return pcoll | '_fixtime' >> beam.MapTuple(lambda k, v: (k, self._fixtime(v)))
 
 
-fs = fsspec.get_filesystem_class('file')()
-target_root = FSSpecTarget(fs, 'modis_LAI')
+fs = s3fs.S3FileSystem(
+    key='', secret='', client_kwargs={'endpoint_url': 'https://nyu1.osn.mghpcc.org'}
+)
+target_root = FSSpecTarget(fs, 'leap-pangeo-pipeline/MODIS_LAI')
+
+
 with beam.Pipeline() as p:
     (
         p
@@ -55,9 +62,11 @@ with beam.Pipeline() as p:
         | OpenWithXarray()
         | FixTime()
         | StoreToZarr(
-            target_root='.',
-            # TODO: Determine chunking
+            target_root=target_root,
+            target_chunks={'time': 100, 'lat': 360, 'lon': 720},
             store_name='MODIS_LAI.zarr',
             combine_dims=pattern.combine_dim_keys,
         )
+        | ConsolidateDimensionCoordinates()
+        | ConsolidateMetadata()
     )
