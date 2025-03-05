29e2d55336a5f7d48f296c6d8ea0ed1ca31eaa3f
norlandrhagen <norlandrhagen@gmail.com>
2025-03-05 13:09:11 -0700

    changed to dask recipe

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,86 +1,61 @@
-"""
-MODIS-LAI
-"""
+# switched to dask 'recipe'
+# input files were transfered in `transfer.sh` to LEAP OSN due to very slow server
 
-# the modified MOD15A2GFS version sourced from nasa data is provided here by BNU data link: http://globalchange.bnu.edu.cn/research/laiv061#usage
-# data managment issue: https://github.com/leap-stc/data-management/issues/188
-from dataclasses import dataclass
-
-import apache_beam as beam
-
-# from leap_data_management_utils.data_management_transforms import (
-#     get_catalog_store_urls,
-# )
 import numpy as np
-import pandas as pd
-import s3fs
 import xarray as xr
 from obstore.fsspec import AsyncFsspecStore
 from obstore.store import S3Store
-from pangeo_forge_recipes.patterns import pattern_from_file_sequence
-from pangeo_forge_recipes.storage import FSSpecTarget
-from pangeo_forge_recipes.transforms import (
-    StoreToZarr,
-)
+import pandas as pd
+from dask.distributed import Client
+import s3fs
 
-# transfered from very slow http server to leap osn with rclone - transfer.sh
+client = Client()
+client
+
 input_urls = [
-    f'https://nyu1.osn.mghpcc.org/leap-pangeo-manual/MODIS_LAI/lai_8-day_0.05_{year}.nc'
+    f"https://nyu1.osn.mghpcc.org/leap-pangeo-manual/MODIS_LAI/lai_8-day_0.05_{year}.nc"
     for year in np.arange(2002, 2020 + 1)
 ]
-
-
-pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
-
-
-@dataclass
-class FixTime(beam.PTransform):
-    """change day of year to datetime"""
-
-    def _fixtime(self, ds: xr.Dataset) -> xr.Dataset:
-        year = ds.time.encoding['source'].split('lai_8-day_0.05_')[1].split('.nc>')[0]
-        dates = pd.to_datetime([f'{year}-{doy}' for doy in ds.time.values], format='%Y-%j')
-        return ds.assign_coords(time=dates)
-
-    def expand(self, pcoll):
-        return pcoll | '_fixtime' >> beam.MapTuple(lambda k, v: (k, self._fixtime(v)))
-
-
-@dataclass
-class OpenXarrayObsStore(beam.PTransform):
-    read_fss: AsyncFsspecStore
-
-    def _obs(self, url: str) -> xr.Dataset:
-        _, suffix = url.rsplit('/', 1)
-        return xr.open_dataset(self.read_fss.open(suffix), engine='h5netcdf')
-
-    def expand(self, pcoll):
-        return pcoll | 'obs' >> beam.MapTuple(lambda k, v: (k, self._obs(v)))
+input_url_suffixes = [url.rsplit("/", 1)[-1] for url in input_urls]
 
 
 read_store = S3Store(
-    'leap-pangeo-manual/MODIS_LAI',
-    aws_endpoint='https://nyu1.osn.mghpcc.org',
-    access_key_id='',
-    secret_access_key='',
+    "leap-pangeo-manual/MODIS_LAI",
+    aws_endpoint="https://nyu1.osn.mghpcc.org",
+    access_key_id="",
+    secret_access_key="",
 )
 read_fss = AsyncFsspecStore(read_store)
 
-fs = s3fs.S3FileSystem(
-    key='', secret='', client_kwargs={'endpoint_url': 'https://nyu1.osn.mghpcc.org'}
-)
-target_root = FSSpecTarget(fs, 'leap-pangeo-pipeline/MODIS_LAI')
 
-with beam.Pipeline() as p:
-    (
-        p
-        | beam.Create(pattern.items())
-        | OpenXarrayObsStore(read_fss=read_fss)
-        | FixTime()
-        | StoreToZarr(
-            target_root=target_root,
-            target_chunks={'time': 100, 'lat': 360, 'lon': 720},
-            store_name='MODIS_LAI.zarr',
-            combine_dims=pattern.combine_dim_keys,
-        )
-    )
+def preprocess(ds: xr.Dataset) -> xr.Dataset:
+    year = ds.time.encoding["source"].split("lai_8-day_0.05_")[1].split(".nc>")[0]
+    dates = pd.to_datetime([f"{year}-{doy}" for doy in ds.time.values], format="%Y-%j")
+    return ds.assign_coords(time=dates)
+
+
+cds = xr.open_mfdataset(
+    [read_fss.open(suffix) for suffix in input_url_suffixes],
+    preprocess=preprocess,
+    parallel=True,
+    coords="minimal",
+    data_vars="minimal",
+    compat="override",
+)
+
+fs = s3fs.S3FileSystem(
+    key="", secret="", client_kwargs={"endpoint_url": "https://nyu1.osn.mghpcc.org"}
+)
+
+mapper = fs.get_mapper("leap-pangeo-pipeline/MODIS_LAI/MODIS_LAI.zarr")
+
+cds.chunk({"time": 100, "lat": 360, "lon": 720}).to_zarr(
+    mapper, mode="w", consolidated=True
+)
+
+# check RT
+
+
+store = "https://nyu1.osn.mghpcc.org/leap-pangeo-pipeline/MODIS_LAI/MODIS_LAI.zarr"
+ds = xr.open_dataset(store, engine="zarr", chunks={})
+ds.isel(time=0).lai.plot()
